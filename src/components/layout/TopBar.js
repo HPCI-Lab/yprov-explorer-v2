c1ef1bc2780062a2bf6fc322c55b871ed84cf3fa
evelin.begher <evelin.begher@studenti.unitn.it>
2025-12-03 08:36:36 +0100

    start catalog

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -1,11 +1,13 @@
 import { Flex, Input, IconButton } from "@chakra-ui/react";
 import { SunIcon } from "@chakra-ui/icons";
-import { Image } from "@chakra-ui/react"
+import { Image } from "@chakra-ui/react";
+
 /*
 TopBar.js: Top bar component that contains main elements for navigation:
 - searchbar for searching the nodes on the graph
 - theme toggler button for changing the theme on preferences
 */
+
 export default function TopBar() {
     return (
         <Flex
@@ -19,7 +21,7 @@ export default function TopBar() {
         >
             {/*Logo container*/}
             <Flex align="center" justify="center" w="70px">
-                <Image src="logo.png" boxSize="40px" borderRadius="xl" aspectRatio={16 / 9} borderLeft="5px white"/>
+                <Image src="/logo.png" boxSize="40px" borderRadius="xl" aspectRatio={16 / 9} borderLeft="5px white"/>
             </Flex>
             <Flex flex="1" justify="center" gap="5"  mx="20px">
                 {/*Search bar*/}
